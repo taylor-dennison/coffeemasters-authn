66f0a0426a052fcaf8bcfb095e24dc2972489470
taylor-dennison <tsd2771@rit.edu>
2024-01-17 10:27:39 -0500

    credential management.

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -4,6 +4,85 @@ import Router from "./Router.js";
 const Auth = {
     isLoggedIn: false,
     account: null,
+    postLogin: (response, user) => {
+        if (response.ok) {
+            Auth.isLoggedIn = true;
+            Auth.account = user;
+            Auth.updateStatus();
+            Router.go("/account");
+        } else {
+            alert(response.message)
+        }
+
+        //Credential Management API Storage
+        //this will store the credentials in the browser for auto login
+        /* **NOTE: PasswordCredential API is not supported in Safari yet, as a whole.
+              It will work with the WebAuthn API (AKA: Passkeys), but not the PasswordCredential API for username/passwords.
+              This if block will determine whether or not we are on a chromium browser or not,
+              since this will be falsy in Safari.
+        */
+        if (window.PasswordCredential && user.password) {
+            const credentials = new PasswordCredential({
+                id: user.email,
+                name: user.name, //name is optional for PasswordCredential.
+                password: user.password,
+            });
+        }
+    },
+    register: async (event) => {
+        event.preventDefault();
+        const user = {
+            name: document.querySelector("#register_name").value,
+            email: document.querySelector("#register_email").value,
+            password: document.querySelector("#register_password").value,
+        };
+        const response = await API.register(user);
+        if (response.ok) {
+            Auth.postLogin(response, user);
+        } else {
+            alert(response.message);
+        }
+    },
+    login: async (event) => {
+        if (event) event.preventDefault(); //autologin does not have an event
+        const credentials = {
+            email: document.querySelector("#login_email").value,
+            password: document.querySelector("#login_password").value,
+        };
+        //log in
+        const response = await Auth.login(credentials); 
+        //using the response to grab the name, we can create the user object here. 
+        Auth.postLogin(response, {
+            ...credentials,
+            name: response.name,
+        });
+
+    },
+    logout: async (event) => {
+        event.preventDefault();
+        Auth.isLoggedIn = false;
+        Auth.account = null;
+        Auth.updateStatus();
+        Router.go("/");
+
+        // instruct the PasswordCredential API to forget the credentials
+        if (window.PasswordCredential) {
+            navigator.credentials.preventSilentAccess();
+        }
+    }, 
+    autoLogin: async () => {
+        if (window.PasswordCredential) {
+            const credentials = await navigator.credentials.get({password: true})
+            //depending on the use case, we can just call Auth.login(credentials) here,
+            //or we can auto fill the login form and let the user click the login button.
+            document.querySelector("login_email").value = credentials.id;
+            document.querySelector("login_password").value = credentials.password;
+
+            //if we do call the login method, since we are not passing an event
+            //here, we need to check if an event exists, as done above.
+
+        }
+    },
     updateStatus() {
         if (Auth.isLoggedIn && Auth.account) {
             document.querySelectorAll(".logged_out").forEach(
@@ -34,6 +113,9 @@ const Auth = {
     },
 }
 Auth.updateStatus();
+//attempt autologin if user has previously logged in and credentials
+//are stored in the password manager
+Auth.autoLogin();
 
 export default Auth;
 
--- server.js
+++ server.js
@@ -1,42 +1,94 @@
-import express from 'express'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+import express from 'express';
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
 import * as url from 'url';
 import bcrypt from 'bcryptjs';
 import * as jwtJsDecode from 'jwt-js-decode';
-import base64url from "base64url";
+import base64url from 'base64url';
 import SimpleWebAuthnServer from '@simplewebauthn/server';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 const adapter = new JSONFile(__dirname + '/auth.json');
 const db = new Low(adapter);
 await db.read();
-db.data ||= { users: [] }
+db.data ||= { users: [] };
 
-const rpID = "localhost";
-const protocol = "http";
+const rpID = 'localhost';
+const protocol = 'http';
 const port = 5050;
 const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 // ADD HERE THE REST OF THE ENDPOINTS
+const findUser = (email) => {
+  const results = db.data.users.filter((u) => u.email === email);
+  if (results.length === 0) {
+    return undefined;
+  }
+  return results[0];
+};
 
+app.post('/auth/login', (req, res) => {
+  const userFound = foundUser(req.body.email);
+  if (!userFound) {
+    res.status(401).send({ok: false, message: 'Credentials are wrong'});
+    return;
+  }
 
+  //we have found the user. Check the password
+  const passwordMatch = bcrypt.compareSync(req.body.password, userFound.password);
+  if (!passwordMatch) {
+    res.status(401).send({ok: false, message: 'Credentials are wrong'});
+    return;
+  }
 
-app.get("*", (req, res) => {
-    res.sendFile(__dirname + "public/index.html"); 
+  //we have a valid user and password. 
+  return res.status(200).send({ok: true, name: userFound.name, email: userFound.email});
+
+})
+
+app.post('/auth/register', (req, res) => {
+  //TODO: DATA VALIDATION + SANITIZATION
+  //Also, we confirm users email addresses by sending them an email with a link to confirm their email address
+
+  const salt = bcrypt.genSaltSync(10); 
+  //there is also an async version of this function
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  };
+  //check if user already exists
+  const userFound = findUser(user.email);
+
+  if (userFound) {
+    res.status(400).send({ok: false, message: 'User already exists'});
+    return;
+  } else {
+    //user is new. 
+    db.data.users.push(user);
+    db.write();
+    res.status(201).send({ok: true, message: 'User registered'});
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + 'public/index.html');
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
-
